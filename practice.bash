8058e6c3e69e39ad45f22d3db01d267e34abdedd
noor <43285313+normAlask@users.noreply.github.com>
2019-10-07 21:55:40 -0500

    Add files via upload

---
+++ practice.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+if [ $# -eq 0 ]; then
+    read -p "Enter Num1:  " num1
+    read -p "Enter num2:  " num2
+    fi
+    if [ $# -eq 1 ]; then
+        read -p "Enter Num2:  " num2
+        fi
+     while [ $num2 -eq 0 ]; do
+        read -p "Enter something thats not 0 bitch." num2
+    done
+
+
+
+go=0
+while [ $go ]; do
+    echo " enter what you what to do nigga"
+    echo " A) sum"
+    echo " b) product" 
+    echo " c) diffrence" 
+    echo " d) div" 
+    echo " e) mod"
+    echo " f) Power"
+    if ! read ans; then
+         # got EOF
+          break
+           fi
+case "$ans" in    
+
+ [Aa])let sum=$num1+$num2
+
+echo "sum = $sum"
+;;
+[Bb])let prod=$num1*$num2
+echo "product = $prod"
+;;
+[Cc])let diff=$num1-$num2
+echo "the diff = $diff"
+;;
+ [Dd])let div=$num1/$num2
+
+echo "div = $div"
+;;
+[Ee])let mod=$num1%$num2
+echo "mod = $mod"
+;;
+[Ff])let power=$num1**$num2
+echo "power = $power"
+;;
+*) echo "pick one of this option nig nog"
+esac
+done
+
